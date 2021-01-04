2836fbe6d8b0934deaf01929f755a6ce0ae722c0
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-04 11:14:28 +0000

    added the contacts titles

--- personal/admin.py
+++ personal/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+
 from .models import Personal
+from .models import Address
+from .models import Title
+from .models import PersonalTitle
 
 # Register your models here.
 
 
 admin.site.register(Personal)
+admin.site.register(Title)
+admin.site.register(Address)
+admin.site.register(PersonalTitle)
