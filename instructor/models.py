42872803ebbb5bf31dfc5b95f73494c3644935d6
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-18 23:24:24 +0000

    added instructor models to project

--- instructor/models.py
+++ instructor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
+
+from personal.models import Personal
 
 # Create your models here.
+
+class Instructor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    personal = models.ForeignKey(Personal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.personal.__str__()
