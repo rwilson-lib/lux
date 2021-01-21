c17c2f061d7d1b0949442ec1b43e066ebe3fb68d
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-21 10:08:25 +0000

    added student course and registration details to model

--- institution/models.py
+++ institution/models.py
@@ -4,30 +4,39 @@ import uuid
 
 from instructor.models import Instructor
 
+
+class Institution(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25)
+
+class ISCEDLevel(models.Model):
+    level =  models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    year = models.DateField()
+    description = models.TextField()
+
 class AcademicYear(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     semester = models.CharField(max_length=25)
     start_year = models.DateField()
     end_year = models.DateField()
-
-
-class Institution(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-
 
 class School(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     name = models.CharField(max_length=25)
     motto = models.CharField(max_length=25)
-    type = models.CharField(max_length=255)
+    mascot = models.ImageField()
+    levels = models.ManyToManyField(ISCEDLevel, on_delete=models.CASCADE)
+    year_founded = models.DateField()
 
     def __str__(self):
         return self.name
+
 
 
 class College(models.Model):
@@ -69,10 +78,23 @@ class Room(models.Model):
         return self.name
 
 
+class Class(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25)
+    category = models.CharField(max_length=25)
+
+
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     course = models.CharField(max_length=100)
     code = models.IntegerField(blank=True, null=True)
+    credit_hour = models.PositiveIntegerField(default=1)
+
+
+class InstructorCourse(models.Model):
+    instructor = models.ManyToManyField(Instructor)
+    course = models.ManyToManyField(Course)
+    xp = models.IntegerField(blank=True, null=True)
 
 
 class Enroll(models.Model):
@@ -82,9 +104,23 @@ class Enroll(models.Model):
     academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
 
 
+class ClassRoom(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25)
+    _class = models.ManyToManyField(Class, on_delete=models.CASCADE)
+    room = models.ManyToManyField(Room, on_delete=models.CASCADE)
+
+
 class Section(models.Model):
     name = models.CharField(max_length=25)
+    classroom = models.ForeignKey(Course, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    #-AcademicYear
     #-Schedule
+
+class SectionEnrollment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    enroll = models.ForeignKey(Enroll, on_delete=models.CASCADE)
