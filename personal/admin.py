ecc82bd969cf2dbdbfbddd1773038c7274d9b6c3
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-26 10:28:39 +0000

    added documents of admin

--- personal/admin.py
+++ personal/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from .models import Personal
 from .models import Address
 from .models import Title
-from .models import PersonalTitle
 from .models import Contact
+from .models import PersonalTitle
+from .models import PersonalAddress
+from .models import PersonalContact
+from .models import Document
 
 # Register your models here.
 
@@ -12,5 +15,8 @@ from .models import Contact
 admin.site.register(Address)
 admin.site.register(Contact)
 admin.site.register(Personal)
-admin.site.register(PersonalTitle)
 admin.site.register(Title)
+admin.site.register(PersonalTitle)
+admin.site.register(PersonalContact)
+admin.site.register(PersonalAddress)
+admin.site.register(Document)
