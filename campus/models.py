1d6416acbefa1a6bf50af4545b9fc41e2fd7027f
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-28 23:01:39 +0000

    added three new models classroom campusstaff and other

--- campus/models.py
+++ campus/models.py
@@ -8,6 +8,7 @@ import uuid
 
 from personal.models import Address
 from institution.models import Institution
+from assembly.models import Class
 
 from school.models import Staff
 from school.models import Course
@@ -62,10 +63,23 @@ class Room(models.Model):
             )
 
 
+
+class ClassRoom(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    _class = models.ForeignKey(Class, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {}".format(self._class.name, self.room.name)
+
+
 class SchoolCampus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {}".format(self.school.name, self.campus.name)
 
 
 class CollegeCampus(models.Model):
@@ -73,13 +87,23 @@ class CollegeCampus(models.Model):
     college = models.ForeignKey(College, on_delete=models.CASCADE)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} {}".format(self.college.name, self.campus.name)
+
+
+
 class CourseCampus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} {}".format(self.course.name, self.campus.name)
 
-class StaffCampus(models.Model):
+class CampusStaff(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}".format(self.campus.name)
