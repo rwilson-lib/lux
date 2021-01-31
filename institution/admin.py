518d1a9e81b118ce7783c779a8469f4acc0813c5
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-31 19:45:14 +0000

    added Program and ProgramMap to admin

--- institution/admin.py
+++ institution/admin.py
@@ -6,9 +6,13 @@ from .models import InstitutionAddress
 from .models import InstitutionContact
 from .models import InstitutionDocument
 from .models import ISCEDLevel
+from .models import Program
+from .models import ProgramMap
 
 admin.site.register(Institution)
 admin.site.register(ISCEDLevel)
 admin.site.register(InstitutionAddress)
 admin.site.register(InstitutionContact)
 admin.site.register(InstitutionDocument)
+admin.site.register(Program)
+admin.site.register(ProgramMap)
