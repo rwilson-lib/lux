bbe9a1bc099295b5d49ea5f72acd9bf9461a9a4c
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-01-31 19:43:05 +0000

    refactor Level added Program and ProgramMap

--- institution/models.py
+++ institution/models.py
@@ -36,10 +36,61 @@ class InstitutionDocument(models.Model):
 
 
 class ISCEDLevel(models.Model):
-    level =  models.PositiveIntegerField()
-    title = models.CharField(max_length=255)
-    year = models.DateField()
-    description = models.TextField()
+
+
+    class Level(models.TextChoices):
+        PRE_KINDERGARDEN = 'PREK'
+        KINDERGARDEN     = 'KIND'
+        GRADE            = 'GRAD'
+        UNDER_GARDUATE   = 'UNGR'
+        GARDUATE         = 'PGRA'
+        POST_GARDUATE    = 'POST'
+        VOCATIONAL       = 'VOCA'
+        TECHNICAL        = 'TECH'
+
+
+
+    class Status(models.TextChoices):
+        FRESHMAN        = 'FR'
+        SOPHOMORE       = 'SO'
+        JUNIOR          = 'JU'
+        SENIOR          = 'SE'
+
+
+    class Category(models.TextChoices):
+        PRIMARY_EDUCATION       = 'PS'
+        SECONDARY_EDUCATION     = 'SS'
+        POSTSECONDARY_EDUCATION = 'PE'
+        VOCATIONAL_EDUCATION    = 'VS'
+        TECHNICAL_EDUCATION     = 'TS'
+
+    id       = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title    = models.CharField(max_length=50)
+    category = models.CharField(max_length=2, choices=Category.choices,
+                                 blank=True, null=True)
+    level    = models.CharField(max_length=4, choices=Level.choices)
+    grade    = models.CharField(max_length=6)
+    ages     = models.CharField(max_length=6)
+    duration = models.CharField(max_length=6)
 
     def __str__(self):
         return self.title
+
+
+class Program(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25)
+    achievement = models.CharField(max_length=25)
+    duration = models.CharField(max_length=6)
+    description = models.TextField()
+    map         = models.ManyToManyField(ISCEDLevel, through='ProgramMap')
+
+    def __str__(self):
+        return self.name
+
+
+class ProgramMap(models.Model):
+    id      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    step    = models.PositiveIntegerField()
+    path    = models.OneToOneField(ISCEDLevel, on_delete=models.CASCADE)
