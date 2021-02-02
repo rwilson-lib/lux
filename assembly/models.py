bac3da7af48036fee7bebe2e3e57fa181e50b086
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-02-02 10:22:11 +0000

    added __str__ for class

--- assembly/models.py
+++ assembly/models.py
@@ -36,3 +36,6 @@ class Assembly(models.Model):
 class Class(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
