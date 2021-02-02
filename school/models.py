51087507315d7b4cc5262a4d228ecadad801263b
Ramus Jabee Lloyd Wilson <ramuswilson@gmail.com>
2021-02-02 10:07:37 +0000

    Fixed relationship in School modules to prevent duplications

--- school/models.py
+++ school/models.py
@@ -26,11 +26,12 @@ class School(models.Model):
         return "{} -:- {}".format(self.institution.name, self.name)
 
 
+
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     level = models.ManyToManyField(ISCEDLevel)
     title = models.CharField(max_length=100)
-    code = models.PositiveIntegerField(blank=True, null=True)
+    code  = models.PositiveIntegerField(blank=True, null=True)
     credit_hour = models.PositiveIntegerField(default=1)
     instructors = models.ManyToManyField(Instructor, through='CourseInstructor')
 
@@ -42,6 +43,12 @@ class CourseInstructor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+
+
+class SchoolCourse(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
 class College(models.Model):
@@ -60,53 +67,49 @@ class CollegeCourse(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
-class SchoolCourse(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-
 class Enroll(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     student = models.OneToOneField(Student, on_delete=models.CASCADE)
-    school = models.OneToOneField(School, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
     id_number = models.CharField(max_length=25, unique=True)
     # academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return self.id_number
+        return "{} {}".format(self.id_number, self.student)
 
 
-class Postion(models.Model):
-    postion = models.CharField(max_length=25)
+class Position(models.Model):
+    position = models.CharField(max_length=25)
     description = models.TextField()
 
     def __str__(self):
-        return self.postion
+        return self.position
 
 
 class Staff(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    emp_id = models.CharField(max_length=25,unique=True)
+    employee_id = models.CharField(max_length=25,unique=True)
     personal = models.OneToOneField(Personal, on_delete=models.CASCADE)
-    postions = models.ManyToManyField(Postion)
+    positions    = models.ManyToManyField(Position)
+    hired_date  = models.DateField()
+    fully_time  = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.personal.__str__()
+        return "{}".format(self.personal)
 
 
 class StudentStatus(models.Model):
     class YearInSchool(models.TextChoices):
-        FRESHMAN = 'FR', _('Freshman')
+        FRESHMAN  = 'FR', _('Freshman')
         SOPHOMORE = 'SO', _('Sophomore')
-        JUNIOR = 'JR', _('Junior')
-        SENIOR = 'SR', _('Senior')
-        GRADUATE = 'GR', _('Graduate')
+        JUNIOR    = 'JR', _('Junior')
+        SENIOR    = 'SR', _('Senior')
+        GRADUATE  = 'GR', _('Graduate')
 
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
- #    academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    # academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
     enroll = models.ForeignKey(Enroll, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=2,
